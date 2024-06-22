c70e9834012f29f1d60dd550ceb6d2be787abee2
lakruwan1 <134932939+lakruwan1@users.noreply.github.com>
2024-06-22 11:25:24 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text>Hi guys how its going</Text>
+    </View>
+  );
+}
